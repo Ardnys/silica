08bf8c66475ab5bda5db4ad1f58e0f94d65ac965
Mustafa <gameacedamy@gmail.com>
2024-05-29 20:49:38 +0300

    show owners total cost

--- public/script.js
+++ public/script.js
@@ -1,77 +1,92 @@
 // TODO fetch stuff here to display in the ui
 async function fetchPets() {
-	try {
-		const response = await fetch("/api/pets");
-		const pets = await response.json();
-		const petList = document.getElementById("pet-list");
-		pets.forEach((pet) => {
-			const listItem = document.createElement("li");
-			listItem.textContent = `${pet.pet_name} (Microchip: ${pet.microchip_number})`;
-			listItem.addEventListener("click", () =>
-				SelectPetAndOwner({
-					owner_name: pet.owner_name,
-					pet_name: pet.pet_name,
-				})
-			);
-			petList.appendChild(listItem);
-		});
-	} catch (error) {
-		console.error("Error fetching pets:", error);
-	}
+  try {
+    const response = await fetch("/api/pets");
+    const pets = await response.json();
+    const petList = document.getElementById("pet-list");
+    pets.forEach((pet) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = `${pet.pet_name} (Microchip: ${pet.microchip_number})`;
+      listItem.addEventListener("click", () =>
+        SelectPetAndOwner({
+          owner_name: pet.owner_name,
+          pet_name: pet.pet_name,
+        })
+      );
+      petList.appendChild(listItem);
+    });
+  } catch (error) {
+    console.error("Error fetching pets:", error);
+  }
 }
 
 async function fetchOwners() {
-	try {
-		const response = await fetch("/api/owners");
-		const owners = await response.json();
-		const ownerList = document.getElementById("owner-list");
-		owners.forEach((owner) => {
-			const listItem = document.createElement("li");
-			listItem.textContent = `Name: ${owner.name} (Phone: ${owner.phone_number})`;
-			listItem.addEventListener("click", async () => {
-				const search_box = document.getElementById("owner-input");
-				search_box.value = owner.name;
-				await fetchOwnerPet();
-			});
-			ownerList.appendChild(listItem);
-		});
-	} catch (error) {
-		console.error("Error fetching owners:", error);
-	}
+  try {
+    const response = await fetch("/api/owners");
+    const owners = await response.json();
+    const ownerList = document.getElementById("owner-list");
+    owners.forEach((owner) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = `Name: ${owner.name} (Phone: ${owner.phone_number})`;
+      listItem.addEventListener("click", async () => {
+        const search_box = document.getElementById("owner-input");
+        search_box.value = owner.name;
+        await fetchOwnerPet();
+      });
+      ownerList.appendChild(listItem);
+    });
+  } catch (error) {
+    console.error("Error fetching owners:", error);
+  }
+}
+async function fetchTotalPrices() {
+  try {
+    const response = await fetch("/api/owner_totals");
+    const ownerPrices = await response.json();
+    const ownerPriceList = document.getElementById("owner-total-list");
+    ownerPrices.forEach((owner) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = `Name: ${owner.name} (Total Price: ${owner.total_price}$)`;
+      ownerPriceList.appendChild(listItem);
+    });
+  } catch (error) {
+    console.error("Error fetching owners:", error);
+  }
 }
 
 async function fetchOwnerPet() {
-	try {
-		const oname_text = document.getElementById("owner-input").value;
-		const response = await fetch(
-			"/api/owner_pet?" + new URLSearchParams({ owner_name: oname_text })
-		);
-		const owners = await response.json();
-		const ownerList = document.getElementById("owner-pet-list");
-		ownerList.innerHTML = "";
-		owners.forEach((owner) => {
-			const listItem = document.createElement("li");
-			listItem.textContent = `Name: ${owner.owner_name} (Pet: ${owner.pet_name})`;
-			listItem.addEventListener("click", () => SelectPetAndOwner(owner));
-			ownerList.appendChild(listItem);
-		});
-	} catch (error) {
-		console.error("Error fetching owners:", error);
-	}
+  try {
+    const oname_text = document.getElementById("owner-input").value;
+    const response = await fetch(
+      "/api/owner_pet?" + new URLSearchParams({ owner_name: oname_text })
+    );
+    const owners = await response.json();
+    const ownerList = document.getElementById("owner-pet-list");
+    ownerList.innerHTML = "";
+    owners.forEach((owner) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = `Name: ${owner.owner_name} (Pet: ${owner.pet_name})`;
+      listItem.addEventListener("click", () => SelectPetAndOwner(owner));
+      ownerList.appendChild(listItem);
+    });
+  } catch (error) {
+    console.error("Error fetching owners:", error);
+  }
 }
 
 // triggered when a pet is selected from the list
 function SelectPetAndOwner(content) {
-	let div = document.getElementById("selected-pet-section");
-	div.hidden = false;
-	let owner_name_p = document.getElementById("owner-name-v");
-	let pet_name_p = document.getElementById("pet-name-v");
-	owner_name_p.innerHTML = content.owner_name;
-	pet_name_p.innerHTML = content.pet_name;
+  let div = document.getElementById("selected-pet-section");
+  div.hidden = false;
+  let owner_name_p = document.getElementById("owner-name-v");
+  let pet_name_p = document.getElementById("pet-name-v");
+  owner_name_p.innerHTML = content.owner_name;
+  pet_name_p.innerHTML = content.pet_name;
 }
 
 document.addEventListener("DOMContentLoaded", fetchPets);
 document.addEventListener("DOMContentLoaded", fetchOwners);
+document.addEventListener("DOMContentLoaded", fetchTotalPrices);
 
 const owner_pet_button = document.getElementById("owner-pet-search");
 owner_pet_button.addEventListener("click", fetchOwnerPet);
--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const PORT = 3000;
 // TODO express server here
 
 const client = new Client({
-	user: "postgres",
-	host: "localhost",
-	database: "vet-db",
-	password: process.env.POSTGRE_PASSWORD,
-	port: "5432",
+  user: "postgres",
+  host: "localhost",
+  database: "vet-db",
+  password: process.env.POSTGRE_PASSWORD,
+  port: "5432",
 });
 
 client.connect();
@@ -20,71 +20,92 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Add fields as necessary
 app.get("/api/pets", (req, res) => {
-	client.query(
-		`SELECT o.name as owner_name, p.name as pet_name, p.microchip_number 
+  client.query(
+    `SELECT o.name as owner_name, p.name as pet_name, p.microchip_number 
 			      FROM pet as p, owner as o 
 				  WHERE p.owner_id = o.owner_id`,
-		(err, result) => {
-			if (err) {
-				res.status(500).send(err);
-			} else {
-				res.json(result.rows);
-			}
-		}
-	);
+    (err, result) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.json(result.rows);
+      }
+    }
+  );
 });
 
 app.get("/api/owners", (req, res) => {
-	client.query("SELECT * FROM owner", (err, result) => {
-		if (err) {
-			res.status(500).send(err);
-		} else {
-			res.json(result.rows);
-		}
-	});
+  client.query("SELECT * FROM owner", (err, result) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(result.rows);
+    }
+  });
+});
+
+app.get("/api/owner_totals", (req, res) => {
+  client.query(
+    `
+  SELECT o.name AS name, SUM(v2.price) AS total_price
+  FROM owner o
+  JOIN pet p ON o.owner_id = p.owner_id
+  JOIN vaccination v1 ON v1.pet_id = p.pet_id
+  JOIN vaccine v2 ON v1.vaccine_id = v2.vaccine_id
+  GROUP BY o.owner_id
+  ORDER BY total_price DESC;
+`,
+    (err, result) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.json(result.rows);
+      }
+    }
+  );
 });
 
 app.get("/api/pet_owner", (req, res) => {
-	const pet_id = req.query.pet_id;
-	let query_string = `select o.name as owner_name
+  const pet_id = req.query.pet_id;
+  let query_string = `select o.name as owner_name
 			from public.pet as p, public.owner as o
 			where p.owner_id = o.owner_id `;
-	if (pet_id) {
-		query_string += `and p.pet_id = $1;`;
-	}
-	client.query(query_string, [pet_id], (err, result) => {
-		if (err) {
-			res.status(500).send(err);
-			console.log("what: " + err);
-		} else {
-			res.json(result.rows);
-		}
-	});
+  if (pet_id) {
+    query_string += `and p.pet_id = $1;`;
+  }
+  client.query(query_string, [pet_id], (err, result) => {
+    if (err) {
+      res.status(500).send(err);
+      console.log("what: " + err);
+    } else {
+      res.json(result.rows);
+    }
+  });
 });
 
 app.get("/api/owner_pet", (req, res) => {
-	const owner_name = req.query.owner_name;
-	console.log("owner name: " + owner_name);
-	let query_string = `select o.name as owner_name, p.name as pet_name
+  const owner_name = req.query.owner_name;
+  console.log("owner name: " + owner_name);
+  let query_string = `select o.name as owner_name, p.name as pet_name
 					from owner as o, pet as p 
 					where p.owner_id = o.owner_id `;
-	if (owner_name) {
-		query_string += `and o.name = $1;`;
-	}
-	console.log("query str: " + query_string);
-	client.query(query_string, [owner_name], (err, result) => {
-		if (err) {
-			res.status(500).send(err);
-			console.log("what: " + err);
-		} else {
-			res.json(result.rows);
-			console.log("yes");
-		}
-	});
+  if (owner_name) {
+    query_string += `and o.name = $1;`;
+  }
+  console.log("query str: " + query_string);
+  client.query(query_string, [owner_name], (err, result) => {
+    if (err) {
+      res.status(500).send(err);
+      console.log("what: " + err);
+    } else {
+      res.json(result.rows);
+      console.log("yes");
+    }
+  });
 });
 
 app.listen(PORT, () => {
-	console.log(`server is running on port: ${PORT}`);
+  console.log(`server is running on port: ${PORT}`);
 });
 
 // // Create
